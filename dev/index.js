bf6ee2d76c5a013a1dab1bca7eccfd4508586454
yijun <iamejun@163.com>
2017-07-31 10:43:43 +0800

    增加检查是否有新版本功能

--- dev/index.js
+++ dev/index.js
@@ -5,3 +5,23 @@
 let tips = require('./modules/tips/tips')
 
 tips($('#tips'))
+
+$.ajax({
+  url: '/home/newver',
+  type: 'GET',
+  dataType: 'json',
+  data: {
+    
+  }
+})
+.done(function(json) {   
+  if(json.re){
+    if(json.result.isnew){
+      var html = $('<div class="alert alert-dismissible alert-info"><button type="button" class="close" data-dismiss="alert">&times;</button>有新版本！ 新版本号：<span id="newver">' + json.result.newver + '</span></div>')
+      $('#hasnew').html(html)
+    }
+  }
+})
+.fail(function(error) {
+  
+})
--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+let checknew = require('../lib/checknew')
+let jsonresult = require('../models/jsonresult')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +10,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/index2', function(req, res, next) {
   res.render('home/index2', { layout: null });
+});
+
+router.get('/home/newver', function(req, res, next) {
+  
+  checknew.hasNew().then(isnew=>{
+    let resultjson = new jsonresult(true, '', isnew)
+    res.json(resultjson)
+  }).catch(error=>{
+    let resultjson = new jsonresult(false, error.message, null)
+    res.json(resultjson)
+  })
 });
 
 module.exports = router;
